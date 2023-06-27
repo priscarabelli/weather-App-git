4856717d8730a0cb159e1c7e1737232ba4b15ab3
priscarabelli <125477480+priscarabelli@users.noreply.github.com>
2023-06-27 22:00:10 +0100

    making app responsive

--- src/script.js
+++ src/script.js
@@ -46,7 +46,7 @@ function displayForecast(response){
   <div class="forecast-days">${formatDay(forecastDay.time)}</div>  
             <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastDay.condition.icon}.png" 
       alt="" 
-      class="img-fluid"
+      class="img-fluid weekly-icon"
       width="80px">
         <div class="forecast-temp">
             <span class="max-temp">${Math.round(forecastDay.temperature.maximum)}° </span>
